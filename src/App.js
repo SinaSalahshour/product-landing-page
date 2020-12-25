7ac4a7d88915fcbe75a363986778bca70c9e2af0
sinasalahshourr <sina.salashour12@gmail.com>
2020-12-25 00:46:18 -0800

    made some changes and added the about page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Features from "./pages/Features/Features";
 import Home from "./pages/Home";
+import About from './pages/About'
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/features" component={Features} />
+        <Route path="/about" component={About} />
       </Switch>
       <Footer />
     </BrowserRouter>
---
+++ src/pages/About.js
@@ -0,0 +1,43 @@
+import React from "react";
+import ReactPlayer from "react-player/youtube";
+
+import "./About.css";
+
+const About = () => {
+  return (
+    <div className="specs-main-section">
+      <div className="specs-text-section">
+        <h6 className="specs-heading">Key Specs</h6>
+        <ul className="key-specs-list">
+          <li className="specs-list-items">Co-designed with Ed O'Brien</li>
+          <li className="specs-list-items">
+            Seymour Duncan® JB Jr. humbucking bridge pickup, Texas Special
+            single-coil Stratocaster middle pickup, Fernandes® Sustainer neck
+            pickup
+          </li>
+          <li className="specs-list-items">
+            "10/56 V"-shaped maple neck with 9.5"-radius maple fingerboard
+          </li>
+          <li className="specs-list-items">
+            Custom "Flower of Life" neck plate; vintage-style hardware
+          </li>
+          <li className="specs-list-items">Includes deluxe gig bag</li>
+        </ul>
+      </div>
+      <div className="specs-video-wrapper">
+        <ReactPlayer
+          className="specs-video"
+          url="https://www.youtube.com/embed/n-qq8RvFscY"
+          width="100%"
+          height="100%"
+          controls={true}
+        />
+      </div>
+      <button type="submit" className="buy-btn">
+        Buy Now
+      </button>
+    </div>
+  );
+};
+
+export default About;
